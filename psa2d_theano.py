863af23a526cbe4ceac267f3119730ddf412f96b
Qiqi Wang <qwang@radix.trade>
2015-11-18 15:07:48 -0600

    Working on psa2d

--- psa2d_theano.py
+++ psa2d_theano.py
@@ -4,9 +4,16 @@ import theano
 import theano.tensor as T
 
 def _is_like_psa(a):
+    '''
+    Check attributes of psa object
+    '''
     return hasattr(a, 'tensor') and hasattr(a, 'has_ghost'):
 
 def _binary_op(op, a, b):
+    '''
+    Perform operations between arrays with or without ghost cells,
+    which is the core functionality of psa objects.
+    '''
     if _is_like_psarray(a) and _is_like_psarray(b):
         if a.has_ghost == b.has_ghost:  # both have or do not have ghost
             return psa(op(a.tensor, b.tensor), a.has_ghost)
@@ -22,9 +29,16 @@ def _binary_op(op, a, b):
         return op(a, b)
 
 class psa(object):
+    '''
+    Objects involved in computation.
+    '''
     def __init__(self, tensor, has_ghost):
         self.tensor = tensor
         self.has_ghost = has_ghost
+
+    #property
+    def ndim(self):
+        return self.tensor.ndim
 
     def __add__(self, a):
         return _binary_op(oprator.add, self, a)
@@ -59,6 +73,21 @@ class psa(object):
     def __neg__(self):
         return psa(-a.tensor, self.has_ghost)
 
+    def transpose(axes=None):
+        if axes is None:
+            axes = tuple(reversed(range(self.ndim)))
+        return psa(self.tensor.transpose((0, 1) + tuple(i+2 for i in axes)),
+                   self.has_ghost)
+
+    def roll(self, x, shift, axis=None):
+        if axis is None:
+            data = x._data.reshape((self.nx, self.ny, -1))
+            data = self._math.roll(data, shift)
+            data = data.reshape(self._preppend_shape(x.shape, x))
+        else:
+            data = self._math.roll(x._data, shift, axis+2)
+        return self._array(data, x.shape)
+
 def sin(a):
     assert _is_like_psa(a)
     return psa(T.sin(a.tensor), a.has_ghost)
@@ -75,15 +104,12 @@ def copy(a):
     assert _is_like_psa(a)
     return psa(T.copy(a.tensor), a.has_ghost)
 
-    def transpose(self, x, axes=None):
-
-    def roll(self, x, shift, axis=None):
-
 i = psa(T.tensor('float64', (False,) * 2))
 j = psa(T.tensor('float64', (False,) * 2))
 
 def _tensorShape(shape):
     return (3,3) + np.empty(shape).shape
+
 def ones(shape):
     return psa(_tensorShape(shape)), True)
 
@@ -92,4 +118,6 @@ def zeros(shape):
 
 def random(shape):
     return psa(_tensorShape(shape)), False)
+
+
 
