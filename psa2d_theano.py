73317dbc906c56d9e1c5f9b16bb561f0781df38f
Your Name <you@example.com>
2015-12-04 04:02:32 +0000

    Tested transpose, left: roll and reshape

--- psa2d_theano.py
+++ psa2d_theano.py
@@ -18,8 +18,10 @@ def _is_like_psa(a):
 
 def _binary_op(op, a, b):
     '''
-    Perform operations between arrays with or without ghost cells,
-    which is the core functionality of psa objects.
+    Perform operations between arrays with or without ghost cells.
+    When both arrays have ghost cells, the resulting array also has ghost cell.
+    When either array has no ghost cell, the resulting array has no ghost cell.
+    This behavior is the primary functionality this module tries to achieve.
     '''
     def _promote_ndim(at, bt):
         if at.ndim == bt.ndim:
@@ -27,7 +29,6 @@ def _binary_op(op, a, b):
         elif at.ndim < bt.ndim:
             at, bt = bt, at
         assert(at.ndim > bt.ndim)
-        print(at.ndim, bt.ndim)
         pad_dim = T.ones(at.ndim - bt.ndim, int)
         new_shape = T.join(0, bt.shape[0:2], pad_dim, bt.shape[2:])
         return at, T.reshape(bt, new_shape, ndim=at.ndim)
@@ -47,17 +48,25 @@ def _binary_op(op, a, b):
     else:
         return op(a, b)
 
+
+# ============================================================================ #
+#                                                                              #
+# ============================================================================ #
+
 class psa(object):
     '''
-    Objects involved in computation.
+    Objects involved in computation.  It should be used like theano arrays,
+    except functions like sin and exp should use the ones defined in this module
     '''
     def __init__(self, tensor, has_ghost):
         self.tensor = tensor
         self.has_ghost = has_ghost
 
-    #property
+    @property
     def ndim(self):
-        return self.tensor.ndim
+        return self.tensor.ndim - 2
+
+    # --------------------------- operations ------------------------------ #
 
     def __add__(self, a):
         return _binary_op(operator.add, self, a)
@@ -92,6 +101,8 @@ class psa(object):
     def __neg__(self):
         return psa(-a.tensor, self.has_ghost)
 
+    # ------------------------- transformations --------------------------- #
+
     @property
     def T(self):
         return self.transpose()
@@ -116,7 +127,11 @@ class psa(object):
             data = self._math.roll(x._data, shift, axis+2)
         return self._array(data, x.shape)
 
-    # ------------------------------------------------------------------- #
+    def copy(self):
+        return copy(self)
+
+    # ------------------------ neighbor access -------------------------- #
+
     def _assert_has_ghost(self):
         if not self.has_ghost:
             raise ValueError('has no ghost')
@@ -138,13 +153,18 @@ class psa(object):
         self._assert_has_ghost()
         return psa(self.tensor[1:-1,:-2], False)
 
+
+# ============================================================================ #
+#                            mathematical functions                            #
+# ============================================================================ #
+
 def sin(a):
     assert _is_like_psa(a)
     return psa(T.sin(a.tensor), a.has_ghost)
 
 def cos(a):
     assert _is_like_psa(a)
-    return psa(T.sin(a.tensor), a.has_ghost)
+    return psa(T.cos(a.tensor), a.has_ghost)
 
 def exp(a):
     assert _is_like_psa(a)
@@ -152,7 +172,13 @@ def exp(a):
 
 def copy(a):
     assert _is_like_psa(a)
-    return psa(T.copy(a.tensor), a.has_ghost)
+    return psa(a.tensor, a.has_ghost)
+    # return psa(T.copy(a.tensor), a.has_ghost)
+
+
+# ============================================================================ #
+#                              atomic psa arrays                               #
+# ============================================================================ #
 
 i = psa(T.tensor('float64', (False,) * 2), True)
 j = psa(T.tensor('float64', (False,) * 2), True)
@@ -160,6 +186,11 @@ j = psa(T.tensor('float64', (False,) * 2), True)
 def ij_np(i0, i1, j0, j1):
     return np.outer(np.arange(i0, i1), np.ones(j1 - j0, int)), \
            np.outer(np.ones(i1 - i0, int), np.arange(j0, j1))
+
+
+# ============================================================================ #
+#                          compiling into theano function                      #
+# ============================================================================ #
 
 def psa_compile(func, inputs=(), args=(), argv={}, with_ij=True):
     np2theano = lambda a: T.Tensor('float64', (False,) * a.ndim)()
@@ -184,6 +215,7 @@ def psa_compile(func, inputs=(), args=(), argv={}, with_ij=True):
     return theano.function(theano_inputs, theano_outputs,
             on_unused_input='ignore')
 
+
 # ============================================================================ #
 #                                 unit tests                                   #
 # ============================================================================ #
@@ -199,7 +231,8 @@ class TestOperators(unittest.TestCase):
 
     def testNonlinear(self):
         def nlLaplacian(a):
-            return (a.x_p + a.x_m + a.y_p + a.y_m - 4 * a) * exp(a) / sin(a)
+            lapla = a.x_p + a.x_m + a.y_p + a.y_m - 4 * a
+            return lapla * exp(a) / sin(a) * cos(a)
         a = np.ones([4,5])
         f = psa_compile(nlLaplacian, a, with_ij=False)
         self.assertEqual(f(a).shape, (2,3))
@@ -246,6 +279,28 @@ class TestIJ(unittest.TestCase):
         fa = f(*ij_np(0,4,0,6))
         self.assertEqual(fa.shape, (2,4))
         self.assertAlmostEqual(abs(fa).max(), 0)
+
+class TestTransforms(unittest.TestCase):
+    def testTranspose(self):
+        def xtm1(a):
+            return a.T - 1
+        a = np.ones([4,5,2,3])
+        f = psa_compile(xtm1, a, with_ij=False)
+        fa = f(a)
+        self.assertEqual(fa.shape, (4,5,3,2))
+        self.assertAlmostEqual(abs(fa).max(), 0)
+
+        def xtp1(a):
+            return a.transpose([2,3,1,0]) + 1
+        a = -np.ones([2,3,4,5,6,7])
+        f = psa_compile(xtp1, a, with_ij=False)
+        fa = f(a)
+        self.assertEqual(fa.shape, (2,3,6,7,5,4))
+        self.assertAlmostEqual(abs(fa).max(), 0)
+
+# ============================================================================ #
+#                                                                              #
+# ============================================================================ #
 
 if __name__ == '__main__':
     unittest.main()
