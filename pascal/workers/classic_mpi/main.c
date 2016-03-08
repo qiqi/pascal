3a1c232eb5147bde6ee63e510c9a164cda3e221f
Qiqi Wang <qiqi@mit.edu>
2016-03-08 18:41:42 -0500

    Working on worker

--- pascal/workers/classic_mpi/main.c
+++ pascal/workers/classic_mpi/main.c
@@ -2,27 +2,68 @@
 #include<stdint.h>
 #include<stdlib.h>
 #include<string.h>
+#include<unistd.h>
+#include<dlfcn.h>
 #include<mpi.h>
-
-#define MAX_VARIABLES 10
-#define NUM_INPUTS 4
+#include<sys/stat.h>
 
 MPI_Comm command_comm;
 
 struct {
     uint64_t i0, i1, j0, j1;
     uint64_t x_m, x_p, y_m, y_p;
-} worker_position;
+} worker_const_position;
 
-void recv_next_job(uint64_t* p_num_step, float* p_workspace)
+typedef struct {
+    uint64_t max_vars, num_inputs, max_steps, step_func_bytes;
+    float * p_workspace;
+    void * p_shared_lib;
+    void (*step_func)(uint64_t i0, uint64_t i1, uint64_t j0, uint64_t j1);
+} job_t;
+
+void delete_job(job_t job)
 {
-    const uint64_t i0 = worker_position.i0;
-    const uint64_t i1 = worker_position.i1;
-    const uint64_t j0 = worker_position.j0;
-    const uint64_t j1 = worker_position.j1;
-    MPI_Recv(p_num_step, 1, MPI_UINT64_T, 0,
-             MPI_ANY_TAG, command_comm, MPI_STATUS_IGNORE);
-    uint64_t half_workspace = MAX_VARIABLES * (i1 - i0 + 2) * (j1 - j0 + 2);
+    free(job.p_workspace);
+}
+
+void receive_job_step_func(job_t job)
+{
+    char * step_func_bin = malloc(job.step_func_bytes);
+    MPI_Bcast(step_func_bin, job.step_func_bytes, MPI_CHAR, 0, command_comm);
+    // write into file
+    char path[2048];
+    getcwd(path, sizeof(path));
+    strcat(path, "/job.so");
+    FILE * f = fopen(path, "wb");
+    f.write(step_func_bin);
+    fchmod(f, 0777);
+    fclose(f);
+    // load file
+    job.p_shared_lib = dlopen(path, RTLD_LAZY);
+    if (p_dl == NULL) {
+        fprintf(stderr, "Failed to load so.so\n");
+        exit(-1);
+    }
+    job.step_func = dlsym(job.p_shared_lib, "step_func");
+    free(step_func_bin);
+}
+
+void allocate_job_workspace(job_t job)
+{
+    uint64_t i0 = worker_const_position.i0;
+    uint64_t i1 = worker_const_position.i1;
+    uint64_t j0 = worker_const_position.j0;
+    uint64_t j1 = worker_const_position.j1;
+    uint64_t num_floats = job.max_vars * 2 * (i1 - i0 + 2) * (j1 - j0 + 2);
+    job.p_workspace = (float*) malloc(sizeof(float) * num_floats);
+}
+
+job_t recv_job()
+{
+    job_t job;
+    MPI_Bcast(&job, 4, MPI_UINT64_T, 0, command_comm);
+    allocate_job_workspace(job);
+    uint64_t half_workspace = job.max_vars * (i1 - i0 + 2) * (j1 - j0 + 2);
     float * p_recv = p_workspace + half_workspace;
     const uint64_t input_size = NUM_INPUTS * (i1 - i0) * (j1 - j0);
     MPI_Recv(p_recv, input_size, MPI_FLOAT, 0,
@@ -43,12 +84,8 @@ int main(int argc, char** argv)
     MPI_Init(&argc, &argv);
     MPI_Comm_get_parent(&command_comm);
     assert(command_comm != MPI_COMM_NULL);
-    MPI_Recv(&worker_position, 8, MPI_UINT64_T, 0,
+    MPI_Recv(&worker_const_position, 8, MPI_UINT64_T, 0,
              MPI_ANY_TAG, command_comm, MPI_STATUS_IGNORE);
-    uint64_t num_floats = MAX_VARIABLES * 2
-         * (worker_position.i1 - worker_position.i0 + 2)
-         * (worker_position.j1 - worker_position.j0 + 2);
-    float * p_workspace = (float*) malloc(sizeof(float) * num_floats);
     // next job
     uint64_t num_steps;
     recv_next_job(&num_steps, p_workspace);
